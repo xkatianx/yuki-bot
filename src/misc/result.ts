8e00db401f123d31e278b959746bcb291a11daba
foo <example@foo.com>
2024-02-17 22:38:01 +0800

    Result.orElseAsync()

--- src/misc/result.ts
+++ src/misc/result.ts
@@ -1,62 +1,73 @@
 /* eslint-disable */
-import { Err, Ok, ErrImpl, OkImpl, Result } from 'ts-results-es'
-import { MyError } from '../error.js'
-// const { Err, Ok, ErrImpl, OkImpl } = pkg
+import { Err, Ok, ErrImpl, OkImpl, Result } from "ts-results-es";
+import { MyError } from "../error.js";
+
 ErrImpl.prototype.andThenAsync = function (_: any) {
-  return this
-}
+  return this;
+};
 OkImpl.prototype.andThenAsync = async function (fn: any) {
-  return await fn(this.value)
-}
+  return await fn(this.value);
+};
 ErrImpl.prototype.mapAsync = function (_: any) {
-  return this
-}
+  return this;
+};
 OkImpl.prototype.mapAsync = async function (fn: any) {
-  return Ok(await fn(this.value))
-}
+  return Ok(await fn(this.value));
+};
+ErrImpl.prototype.orElseAsync = async function (fn: any) {
+  return await fn(this.error);
+};
+OkImpl.prototype.orElseAsync = function (_: any) {
+  return this;
+};
 ErrImpl.prototype.unwrapOrElse = function (fn: any) {
-  return fn(this.error)
-}
+  return fn(this.error);
+};
 OkImpl.prototype.unwrapOrElse = function (_: any) {
-  return this.value
-}
-declare module 'ts-results-es' {
+  return this.value;
+};
+declare module "ts-results-es" {
   interface ErrImpl<E> {
-    andThenAsync(fn: unknown): ErrImpl<E>
-    mapAsync(fn: unknown): ErrImpl<E>
+    orElseAsync<T2>(fn: (err: E) => Promise<OkImpl<T2>>): Promise<OkImpl<T2>>;
+    orElseAsync<E2>(fn: (err: E) => Promise<ErrImpl<E2>>): Promise<ErrImpl<E2>>;
+    orElseAsync<T2, E2>(
+      fn: (err: E) => Promise<Result<T2, E2>>
+    ): Promise<Result<T2, E2>>;
+    andThenAsync(fn: unknown): ErrImpl<E>;
+    mapAsync(fn: unknown): ErrImpl<E>;
     /**
      * Returns the contained `Ok` value or a provided default.
      *
      * (This is the `unwrap_or_else` in rust)
      */
-    unwrapOrElse<T2>(fn: (err: E) => T2): T2
+    unwrapOrElse<T2>(fn: (err: E) => T2): T2;
   }
   interface OkImpl<T> {
-    andThenAsync<T2>(fn: (val: T) => Promise<OkImpl<T2>>): Promise<OkImpl<T2>>
+    orElseAsync(fn: unknown): OkImpl<T>;
+    andThenAsync<T2>(fn: (val: T) => Promise<OkImpl<T2>>): Promise<OkImpl<T2>>;
     andThenAsync<E2>(
       fn: (val: T) => Promise<ErrImpl<E2>>
-    ): Promise<Result<T, E2>>
+    ): Promise<Result<T, E2>>;
     andThenAsync<T2, E2>(
       fn: (val: T) => Promise<Result<T2, E2>>
-    ): Promise<Result<T2, E2>>
-    mapAsync<U>(fn: (val: T) => Promise<U>): Promise<OkImpl<U>>
+    ): Promise<Result<T2, E2>>;
+    mapAsync<U>(fn: (val: T) => Promise<U>): Promise<OkImpl<U>>;
     /**
      * Returns the contained `Ok` value or a provided default.
      *
      * (This is the `unwrap_or_else` in rust)
      */
-    unwrapOrElse(fn: unknown): T
+    unwrapOrElse(fn: unknown): T;
   }
 }
-// export type Result<T, E> = pkg.Result<T, E>
-export { Err, Ok, Result }
+export { Err, Ok, Result };
 
 export async function awaitedResult<T, E>(
   res: Result<T, E>
 ): Promise<Result<Awaited<T>, Awaited<E>>> {
-  if (res.isOk()) return Ok(await res.value)
-  if (res.isErr()) return Err(await res.error)
-  MyError.fatal('results.ts does not work properly.')
+  if (res.isOk()) return Ok(await res.value);
+  if (res.isErr()) return Err(await res.error);
+  MyError.fatal("results.ts does not work properly.");
 }
 
 /* TODO
