74016abd16ba5782fce09d805d3dcd86d6d40dc9
foo <example@foo.com>
2024-02-18 20:45:11 +0800

    add some workaround of ts bug in result.ts

--- src/misc/result.ts
+++ src/misc/result.ts
@@ -33,6 +33,9 @@ declare module "ts-results-es" {
     orElseAsync<T2, E2>(
       fn: (err: E) => Promise<Result<T2, E2>>
     ): Promise<Result<T2, E2>>;
+    orElseAsync<R extends Result<OkContent<R>, ErrContent<R>>>(
+      fn: (err: E) => Promise<R>
+    ): Promise<R>;
     andThenAsync(fn: unknown): ErrImpl<E>;
     mapAsync(fn: unknown): ErrImpl<E>;
     /**
@@ -47,10 +50,13 @@ declare module "ts-results-es" {
     andThenAsync<T2>(fn: (val: T) => Promise<OkImpl<T2>>): Promise<OkImpl<T2>>;
     andThenAsync<E2>(
       fn: (val: T) => Promise<ErrImpl<E2>>
-    ): Promise<Result<T, E2>>;
+    ): Promise<ErrImpl<E2>>;
     andThenAsync<T2, E2>(
       fn: (val: T) => Promise<Result<T2, E2>>
     ): Promise<Result<T2, E2>>;
+    andThenAsync<R extends Result<OkContent<R>, ErrContent<R>>>(
+      fn: (val: T) => Promise<R>
+    ): Promise<R>;
     mapAsync<U>(fn: (val: T) => Promise<U>): Promise<OkImpl<U>>;
     /**
      * Returns the contained `Ok` value or a provided default.
@@ -70,9 +76,35 @@ export async function awaitedResult<T, E>(
   MyError.fatal("results.ts does not work properly.");
 }
 
+type OkContent<T> = T extends OkImpl<infer U> ? U : never;
+type ErrContent<T> = T extends ErrImpl<infer U> ? U : never;
+
+/** This does nothing in JS. Just help TS to know a Result is a Result. */
+export function asResult<T extends Result<OkContent<T>, ErrContent<T>>>(
+  res: T
+): Result<OkContent<T>, ErrContent<T>> {
+  return res;
+}
+
+/** This wraps an (async) Result-returning function and runs it.
+ * This does nothing in JS.
+ * Just helps TS to know the result is a Result.
+ */
+export function asResultFn<T extends Result<OkContent<T>, ErrContent<T>>>(
+  resFn: () => T
+): Result<OkContent<T>, ErrContent<T>>;
+export function asResultFn<T extends Result<OkContent<T>, ErrContent<T>>>(
+  resFn: () => Promise<T>
+): Promise<Result<OkContent<T>, ErrContent<T>>>;
+export function asResultFn<T extends Result<OkContent<T>, ErrContent<T>>>(
+  resFn: () => T | Promise<T>
+):
+  | Result<OkContent<T>, ErrContent<T>>
+  | Promise<Result<OkContent<T>, ErrContent<T>>> {
+  return resFn();
+}
+
 /* TODO
 https://github.com/microsoft/TypeScript/issues/57356
-wait until maybe TS 5.5.0 to replace all the union error type assertion
-by `.mapErr(e => e)`
-and get rid of "as any" and "@ts-ignore"
+wait until maybe TS 5.5.0 to get rid of some asResult()
 */
