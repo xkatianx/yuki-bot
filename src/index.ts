24706bb59be0bcdb04aa3ffbfa601102121a1316
foo <example@foo.com>
2024-02-19 12:32:43 +0800

    and polyfill to node 16

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { polyfill } from "./polyfill.js";
 import { Yuki } from "./discord/yuki/yuki.js";
 import { env } from "./misc/env.js";
 
+polyfill();
 new Yuki(env.DC.TOKEN).login();
---
+++ src/polyfill.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+// @ts-ignore
+Symbol.dispose ??= Symbol("Symbol.dispose");
+// @ts-ignore
+Symbol.asyncDispose ??= Symbol("Symbol.asyncDispose");
+export function polyfill() {}
