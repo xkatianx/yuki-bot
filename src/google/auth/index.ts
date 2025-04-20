6f589226760496a057a31f4180a0fea0d433ef5e
xkatianx <23047821+xkatianx@users.noreply.github.com>
2025-04-20 17:18:25 +0800

    Add unit tests for Google Auth and enhance Google info retrieval

--- src/google/auth/index.ts
+++ src/google/auth/index.ts
@@ -1,9 +1,8 @@
 import "~/misc/env.js";
 
-import {
-  GoogleAuth,
-  type OAuth2Client,
-} from "google-auth-library";
+import { GoogleAuth, type OAuth2Client } from "google-auth-library";
+
+import { drive_v3 } from "@googleapis/drive";
 
 const scopes = [
   "https://www.googleapis.com/auth/drive",
@@ -12,3 +11,16 @@ const scopes = [
 export const gClient = (await new GoogleAuth({
   scopes,
 }).getClient()) as OAuth2Client;
+
+async function myEmail() {
+  const drive = new drive_v3.Drive({ auth: gClient });
+  const res = await drive.about.get({ fields: "user" });
+  return res.data.user?.emailAddress;
+}
+
+const email = await myEmail();
+
+export const myGoogleInfo = {
+  email,
+  projectId: gClient.projectId,
+};
